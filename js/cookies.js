4e1c06ddd4d2cb19103dedfa6902eccc4023fceb
Brent Carton <brent.carton@student.ehb.be>
2019-05-24 15:56:29 +0200

    connect cleaner gemaakt en mailto en tel geadd

--- js/cookies.js
+++ js/cookies.js
@@ -25,21 +25,21 @@ $(function(){
 
     function eersteJaar(cookies, projecten1, i){
         if(cookies[i]<7){
-            $(".connect").append("<h2>"+projecten1[cookies[i]].student+"</h2><h3>"+projecten1[cookies[i]].project+"</h3><p>"+projecten1[cookies[i]].email+"</p><p>"+projecten1[cookies[i]].telefoonnummer+"</p><p>"+projecten1[cookies[i]].studie+"</p>");
+            $(".connect").append("<div class='student'><h2>"+projecten1[cookies[i]].student+"</h2><h3>"+projecten1[cookies[i]].project+"</h3><a id='mail' href='mailto:"+projecten1[cookies[i]].email+"'>"+projecten1[cookies[i]].email+"</a><a id='tel'  href='tel:"+projecten1[cookies[i]].telefoonnummer+"'>"+projecten1[cookies[i]].telefoonnummer+"</a><p>"+projecten1[cookies[i]].studie+"</p></div>");
         }else{
             console.log("Geen 1e jaars aangeduid")
         }
     };
     function tweedeJaar(cookies, projecten2, i){
         if(cookies[i]>6 && cookies[i]<11){
-            $(".connect").append("<h2>"+projecten2[cookies[i]].student+"</h2><h3>"+projecten2[cookies[i]].project+"</h3><p>"+projecten2[cookies[i]].email+"</p><p>"+projecten2[cookies[i]].telefoonnummer+"</p><p>"+projecten2[cookies[i]].studie+"</p>");
+            $(".connect").append("<div class='student'><h2>"+projecten2[cookies[i]].student+"</h2><h3>"+projecten2[cookies[i]].project+"</h3><a id='mail' href='mailto:"+projecten2[cookies[i]].email+"'>"+projecten2[cookies[i]].email+"</a><a id='tel' href='tel:"+projecten2[cookies[i]].telefoonnummer+"'>"+projecten2[cookies[i]].telefoonnummer+"</a><p>"+projecten2[cookies[i]].studie+"</p></div>");
         }else{
             console.log("Geen 2e jaars aangeduid")
         }
     }
     function derdeJaar(cookies, projecten3, i){
         if(cookies[i]>10){
-                $(".connect").append("<h2>"+projecten3[cookies[i]].student+"</h2><h3>"+projecten3[cookies[i]].project+"</h3><p>"+projecten3[cookies[i]].email+"</p><p>"+projecten3[cookies[i]].telefoonnummer+"</p><p>"+projecten3[cookies[i]].studie+"</p>");
+                $(".connect").append("<div class='student'><h2>"+projecten3[cookies[i]].student+"</h2><h3>"+projecten3[cookies[i]].project+"</h3><a id='mail' href=mailto:'"+projecten3[cookies[i]].email+"'>"+projecten3[cookies[i]].email+"</a><a id='tel' href='tel:"+projecten3[cookies[i]].telefoonnummer+"'>"+projecten3[cookies[i]].telefoonnummer+"</a><p>"+projecten3[cookies[i]].studie+"</p></div>");
             }else{
                 console.log("Geen 3e jaars aangeduid")
             }
