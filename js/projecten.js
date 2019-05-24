0391ba6dc54f0affff954fbba0bc9eb03746fed9
Brent Carton <brent.carton@student.ehb.be>
2019-05-24 12:28:04 +0200

    scroll indication

--- js/projecten.js
+++ js/projecten.js
@@ -12,6 +12,7 @@ $(function () {
             DerdeBachelor(data);
             startSelecties();
             Connect();
+            aanduidingScroll();
         }).fail(function (a,b){
             console.log(a,b);
         });
@@ -43,19 +44,19 @@ $(function () {
             /*============================================
                                 1STE
             ============================================*/
-            $(".slideshow-een").show();
+            $(".eerste").show();
             $(this).css("background-color", "#ff0000");
             $(this).css("color", "white");
             /*============================================
                                 2DE
             ============================================*/
-            $(".slideshow-twee").hide();
+            $(".tweede").hide();
             $(".bachelorTwee").css("background-color", "white");
             $(".bachelorTwee").css("color", "#ff0000");
             /*============================================
                                 3DE
             ============================================*/
-            $(".slideshow-drie").hide();
+            $(".derde").hide();
             $(".bachelorDrie").css("background-color", "white");
             $(".bachelorDrie").css("color", "#ff0000");
         });
@@ -69,6 +70,7 @@ $(function () {
             var projecten2 = data[1];
             for (var i in projecten2){
                 console.log(i);
+                $(".tweede").prepend("<a class='dot' href='#' id='"+i+"'></a>");
                 $(".slideshow-twee").append(
                     "<div class='mySlides fade' id='"+i+"'><img src='../Images/"+i+".jpg'><div class='usb-on on"+i+"'><img src='Images/USB-icon.png' id='"+i+"'></div>  <div class='usb-off off"+i+"'><img src='Images/USB-iconZwart.png' id='"+i+"'></div><h1>"+projecten2[i].project+"</h1><h2>"+projecten2[i].student+"</h2><p>"+projecten2[i].pojectbeschrijving+"</p><p>"+projecten2[i].email+"</p><p>"+projecten2[i].telefoonnummer+"</p><p>"+projecten2[i].studie+"</p></div>"
                 );
@@ -80,21 +82,22 @@ $(function () {
             /*============================================
                                 1STE
             ============================================*/
-            $(".slideshow-een").hide();
+            $(".eerste").hide();
             $(".bachelorEen").css("background-color", "white");
             $(".bachelorEen").css("color", "#ff0000");
             /*============================================
                                 2DE
             ============================================*/
-            $(".slideshow-twee").show();
+            $(".tweede").show();
             $(this).css("background-color", "#ff0000");
             $(this).css("color", "white");
             /*============================================
                                 3DE
             ============================================*/
-            $(".slideshow-drie").hide();
+            $(".derde").hide();
             $(".bachelorDrie").css("background-color", "white");
             $(".bachelorDrie").css("color", "#ff0000");
+            aanduidingScroll();
         });
     }
 
@@ -105,6 +108,7 @@ $(function () {
         $(".bachelorDrie").one("click",function () {    
             var projecten3 = data[2];
             for (var i in projecten3){
+                $(".derde").prepend("<a class='dot' href='#' id='"+i+"'></a>");
                 console.log(i);
                 $(".slideshow-drie").append("<div class='mySlides fade' id='"+i+"'><img src='../Images/"+i+".jpg'><div class='usb-on on"+i+"'><img src='Images/USB-icon.png' id='"+i+"'></div>  <div class='usb-off off"+i+"'><img src='Images/USB-iconZwart.png' id='"+i+"'></div><h1>"+projecten3[i].project+"</h1><h2>"+projecten3[i].student+"</h2><p>"+projecten3[i].pojectbeschrijving+"</p><p>"+projecten3[i].email+"</p><p>"+projecten3[i].telefoonnummer+"</p><p>"+projecten3[i].studie+"</p></div>"
                 );
@@ -112,24 +116,26 @@ $(function () {
         });
         $(".bachelorDrie").click(function () {
             Connect();
+            
             /*============================================
                                 1STE
             ============================================*/
-            $(".slideshow-een").hide();
+            $(".eerste").hide();
             $(".bachelorEen").css("background-color", "white");
             $(".bachelorEen").css("color", "#ff0000");
             /*============================================
                                 2DE
             ============================================*/
-            $(".slideshow-twee").hide();
+            $(".tweede").hide();
             $(".bachelorTwee").css("background-color", "white");
             $(".bachelorTwee").css("color", "#ff0000");
             /*============================================
                                 3DE
             ============================================*/
-            $(".slideshow-drie").show();
+            $(".derde").show();
             $(this).css("background-color", "#ff0000");
             $(this).css("color", "white");
+            aanduidingScroll();
         });
     }
 
@@ -159,5 +165,17 @@ $(function () {
         var cookie = "id" + id;
         console.log(cookie);
         Cookies.remove(cookie);
+    }
+    
+    function aanduidingScroll(){
+        if($("div:visible").hasClass("eerste")){
+            console.log("een");
+            
+        }else if($("div:visible").hasClass("tweede")){
+            console.log("twee");
+            
+        }else if($("div:visible").hasClass("derde")){
+            console.log("drie");
+        }
     }
 });
